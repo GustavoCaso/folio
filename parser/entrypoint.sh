884052a4f9a9d333b55a2301c23065cface1a546
Gustavo Caso <gustavocaso@gmail.com>
2026-05-07 22:26:46 +0200

    prepare Docker images for release

--- parser/entrypoint.sh
+++ parser/entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 set -e
 
-exec "$@"
+PUID=${PUID:-1000}
+PGID=${PGID:-1000}
+
+echo "Starting with PUID=$PUID, PGID=$PGID"
+
+if [ "$(id -u folio)" != "$PUID" ] || [ "$(id -g folio)" != "$PGID" ]; then
+    echo "Adjusting user and group IDs..."
+    groupmod -g "$PGID" folio
+    usermod -u "$PUID" folio
+fi
+
+echo "Fixing ownership of /app/models..."
+chown -R "$PUID:$PGID" /app/models
+
+exec gosu folio "$@"
